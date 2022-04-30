f55e09e770e650d2598cd102a4ffa4259e20a4cd
Kylian Lichtensteiger <Kylian.Lichtensteiger@bsi-software.com>
2022-04-30 05:08:38 +0200

    added Singleton pattern

---
+++ src/main/java/dev/srylax/bbbassets/designpatterns/singleton/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Ensure a class only has one instance, and provide a global point of access to it.
+ * <p>
+ * The provided class is only an instance manager, which ensures that there is only one instance registered.
+ * <p>
+ * This may be not applicable to most situations. For a definite implementation, use intellij shortcut {@code geti}
+ * <p>
+ * <img src="doc-files/singleton.png" alt="Singleton pattern">
+ * <p>
+ * <a href="https://java-design-patterns.com/patterns/singleton/">Singleton Pattern</a>
+ */
+package dev.srylax.bbbassets.designpatterns.singleton;
